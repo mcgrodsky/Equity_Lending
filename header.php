4248164f22eaa9bc51194d0fe41bf4afa92c47d6
MiaKoda Grodsky <mcgrodsky@gmail.com>
2016-01-27 12:06:56 -0500

    custom contact form added

--- header.php
+++ header.php
@@ -15,7 +15,7 @@
         'theme_location' => 'main-menu', // Setting up the location for the main-menu, Main Navigation.
         'menu_class' => 'dropdown', //Adding the class for dropdowns
         'container_id' => 'navwrap', //Add CSS ID to the containter that wraps the menu.
-        'fallback_cb' => 'wp_page_menu', //if wp_nav_menu is unavailable, WordPress displays wp_page_menu function, which displays the pages of your blog.
+        'fallback_cb' => 'wp_page_menu', //if wp_nav_menu is unavailable, WordPress displays wp_page_menu function
         ));
         ?>
   </div>
