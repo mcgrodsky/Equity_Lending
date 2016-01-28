23283862abe60f05b51651588da433a5338cae9f
MiaKoda Grodsky <mcgrodsky@gmail.com>
2016-01-27 19:58:24 -0500

    6 ACF added to about page

--- page-about.php
+++ page-about.php
@@ -2,20 +2,41 @@
 <div id ="page-about" class = "clearfix">
   <div class = "about-description">
     <?php
-      if(have_posts()):
-        while(have_posts()):
-          the_post(); ?>
-          <h3><?php the_content(); ?> </h3>
-        <?php endwhile;
-      endif;
+    if(have_posts()):
+      while(have_posts()):
+        the_post(); ?>
+        <h3><?php the_content(); ?> </h3>
+      <?php endwhile;
+    endif;
     ?>
-    <div id = "icon" class = "clearfix">
-      <div class = "icon-div">Rapid </div>
-      <div class = "icon-div">Strong </div>
-      <div class = "icon-div">Knowledgeable </div>
+
+    <!-- ACF for target words -->
+    <div id = "target-header" class = "clearfix">
+      <h2 class = "target-word"><?php the_field('adjective1'); ?></h2>
+      <h2 class = "target-word"><?php the_field('adjective2'); ?></h2>
+      <h2 class = "target-word"><?php the_field('adjective3'); ?></h2>
     </div>
-  </div>
-  <!-- loop to print content  -->
+     <!-- ACF for target images -->
+     <div id = "target-icons" class = "clearfix">
+      <?php
+        $image = get_field('image1');
+        if( !empty($image) ): ?>
+        <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" class = "target-icon" />
+      <?php endif; ?>
+      <?php
+        $image = get_field('image2');
+        if( !empty($image) ): ?>
+        <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" class = "target-icon" />
+      <?php endif; ?>
+      <?php
+        $image = get_field('image3');
+        if( !empty($image) ): ?>
+        <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" class = "target-icon" />
+      <?php endif; ?>
+    </div>
+
+</div>
+<!-- loop to print content  -->
 
 </div>
 <?php get_footer(); ?>
