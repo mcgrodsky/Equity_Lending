609e5809a713c8f67cedb2f1a3df672999ceac20
MiaKoda Grodsky <mcgrodsky@gmail.com>
2016-02-01 10:14:06 -0500

    sticky header

--- header.php
+++ header.php
@@ -3,19 +3,21 @@
     <head>
       <title>Equity Lending, Inc.</title>
       <?php wp_head(); ?>
+      <link href='https://fonts.googleapis.com/css?family=Fira+Sans' rel='stylesheet' type='text/css'>
     </head>
   <body>
     <div class = "wrapper">
-      <div class = "clearfix" id = "header-nav">
-        <a href="<?php echo get_option('about'); ?>/"><div class= "logo"> <img src="http://i.imgur.com/oHYEVI1.jpg" alt="Equity Lending Inc"class = "logo-img" /></div></a>
-        <div id="navigation" class = "clearfix">
-          <?php
-          wp_nav_menu( array(
-          'theme_location' => 'main-menu', // Setting up the location for the main-menu, Main Navigation.
-          'menu_class' => 'dropdown clearfix', //Adding  class for dropdowns
-          'container_id' => 'navwrap', //Add ID to the containter that wraps menu.
-          'fallback_cb' => 'wp_page_menu', //if wp_nav_menu is unavailable, WordPress displays wp_page_menu function
-          ));
-          ?>
+        <div class = "clearfix" id = "header-nav">
+
+          <div id="navigation" class = "clearfix">
+            <a href="<?php echo get_option('about'); ?>/"><div class= "logo"> <img src="http://i.imgur.com/oHYEVI1.jpg" alt="Equity Lending Inc"class = "logo-img" /></div></a>
+            <?php
+            wp_nav_menu( array(
+            'theme_location' => 'main-menu', // Setting up the location for the main-menu, Main Navigation.
+            'menu_class' => 'dropdown clearfix', //Adding  class for dropdowns
+            'container_id' => 'navwrap', //Add ID to the containter that wraps menu.
+            'fallback_cb' => 'wp_page_menu', //if wp_nav_menu is unavailable, WordPress displays wp_page_menu function
+            ));
+            ?>
+          </div>
         </div>
-      </div>
--- page_forms_templates.php
+++ page_forms_templates.php
@@ -2,13 +2,14 @@
 // Template Name: Forms Display
 ?>
 <?php get_header(); ?>
-
+  <h4 class = "form-header"> Please fill out required fields. We will contact you upon form submission </h4>
+<div class = "form">
   <?php if(have_posts()):
     while(have_posts()):
       the_post();?>
       <h2><?php the_content();?></h2>
-
-  <?php endwhile; ?>
+    <?php endwhile; ?>
   <?php endif; ?>
-  <h3> Forms! </h3>
+</div>
+
 <?php get_footer();?>
